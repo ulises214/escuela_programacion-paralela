4b25827600d1de49b37859547ff97672435afdf8
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2022-01-21 07:46:39 -0600

    Script to run program in cluster

---
+++ examen/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -l nodes=3:ppn=2,walltime=10:00:00
+#PBS -N mpi_walle_extra
+#PBS -q staff
+#PBS -d /mnt/zfs-pool/home/cursoHPC13/escuela_programacion-paralela/examen
+#PBS -o mpi_curso_programa.log
+#PBS -j oe
+#PBS -V
+#PBS -S /bin/bash
+source $MODULESHOME/init/bash
+module purge
+module load Compilers/Parallel-Studio-XE-2018
+NPROCS=‘wc -l < $PBS_NODEFILE‘
+cat ${PBS_NODEFILE} | sort -u > $PBS_O_WORKDIR/machines.LINUX
+mpirun -np $NPROCS -machinefile machines.LINUX ./programa > mpi_curso_programa.out
